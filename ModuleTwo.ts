6d2e10842cab16e3b7d97da8edcbb09cd156cb36
lpachonda27 <lpachonda@gmail.com>
2021-09-19 22:47:48 -0500

    Taller 4

--- ModuleTwo.ts
+++ ModuleTwo.ts
@@ -12,6 +12,7 @@ export function restar(valor1:number, valor2:number): number {
 }  
 
 
+
 /* Funciones Boolean */
 export function cat(valor1:boolean, valor2:boolean): boolean {
     return valor1 && valor2;
@@ -24,3 +25,63 @@ export function cat2(valor1:boolean, valor2:boolean): boolean {
 export function cat3(valor1:boolean, valor2:boolean, valor3:boolean, valor4:boolean): boolean {
     return valor1 && valor2 || valor3 && valor4;
 }
+
+
+/*  Funciones array  */
+
+export  let nombres: string[]= ['andres', 'luisa','william','sebastian'];
+nombres.push('luisa');
+
+export let animales: [string,string,string,string] = ['pitbull','golden','beagle','bulldog'];
+
+export let datos: [string,string,number,string] =['Luisa','Pachon',19,'Bogota'];
+
+
+
+/*  Funciones string */
+
+let palabraConcat :string;
+
+export function palabraConcatenar (Name: string, Last: string): string{
+
+    palabraConcat = Name + ' '+ Last; 
+
+    return palabraConcat;
+}
+
+    let cont:number =0;
+export function leernumeros(palabra: string):string{
+
+    let textnumber = palabra;
+
+    for (let index = 0; index < textnumber.length; index++) {
+        if(textnumber[index] == '2' || textnumber[index] == '3'){
+            cont++;
+        }
+    }
+
+    return 'La palabra ingresada tiene '+ cont + ' numeros';
+}
+
+export function convertirminuscula(palabra: string):string{
+
+    return palabra.toLowerCase();
+
+
+}
+
+
+
+/* VARIABLES any */
+
+export let Datoanimal:any = '3';
+ Datoanimal= parseInt(Datoanimal);
+
+export let Datopaciente:any = false;
+Datopaciente = Datopaciente.toString();
+
+export let Datopersonal: any =true;
+Datopersonal = Boolean(Datopersonal);
+
+
+
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import {sumar,operar,restar,cat,cat2,cat3} from "./ModuleTwo";
+import {sumar,operar,restar,cat,cat2,cat3,nombres,animales,datos, convertirminuscula, leernumeros, palabraConcatenar, Datopersonal, Datopaciente, Datoanimal} from "./ModuleTwo";
 
 console.log(operar(3, 7, (x: number,y: number): number => {
     return x+y;
@@ -10,3 +10,19 @@ console.log(restar(9,-4));
 console.log(cat(true,false));
 console.log(cat2(false,true));
 console.log(cat3(false,true,true, false));
+
+
+console.log(nombres);
+console.log(animales);
+console.log('Nombres: ' + datos[0]+ ' '+datos[1]+"\nedad: "+datos[2]+ "\nCiudad: "+ datos[3]);
+
+
+
+console.log(palabraConcatenar('name','last'));
+console.log(convertirminuscula('LUISAPACHON'));
+console.log(leernumeros('1234567676'));
+
+
+console.log(Datoanimal);
+console.log(Datopaciente);
+console.log(Datopersonal);
